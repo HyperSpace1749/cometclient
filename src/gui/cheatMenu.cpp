64377ba67da20cc737b62002d36671468b395cb7
VoidCosmos <voidcosmos4612@gmail.com>
2022-11-20 02:37:59 +0530

    added CometClient name to cheatMenu.cpp

--- src/gui/cheatMenu.cpp
+++ src/gui/cheatMenu.cpp
@@ -142,7 +142,7 @@ void CheatMenu::draw(video::IVideoDriver *driver, bool show_debug)
 	CHEAT_MENU_GET_SCRIPTPTR
 
 	if (!show_debug)
-		drawEntry(driver, "Dragonfireclient", 0, false, false,
+		drawEntry(driver, "CometClient", 0, false, false,
 				CHEAT_MENU_ENTRY_TYPE_HEAD);
 	int category_count = 0;
 	for (auto category = script->m_cheat_categories.begin();
